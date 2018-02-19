b13306ec825001e7290fdf5accafd6046a145b43
thorbenkohler <thorben.kohler@gmail.com>
2018-02-19 16:04:46 +0100

    introduced project log category and the command to trace functions.

--- SideScroller/Source/SideScroller/SideScroller.h
+++ SideScroller/Source/SideScroller/SideScroller.h
@@ -3,3 +3,34 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+
+DECLARE_LOG_CATEGORY_EXTERN(SideScrollerLog, Log, All);
+
+
+#define NETMODE_WORLD (((GEngine == nullptr) || (GetWorld() == nullptr)) ? TEXT("") \
+        : (GEngine->GetNetMode(GetWorld()) == NM_Client) ? TEXT("[Client] ") \
+        : (GEngine->GetNetMode(GetWorld()) == NM_ListenServer) ? TEXT("[ListenServer] ") \
+        : (GEngine->GetNetMode(GetWorld()) == NM_DedicatedServer) ? TEXT("[DedicatedServer] ") \
+        : TEXT("[Standalone] "))
+
+#if _MSC_VER
+#define FUNC_NAME    TEXT(__FUNCTION__)
+#else // FIXME - GCC?
+#define FUNC_NAME    TEXT(__func__)
+#endif
+
+#define TRACE(Format, ...) \
+{ \
+    SET_WARN_COLOR(COLOR_CYAN);\
+    const FString Msg = FString::Printf(TEXT(Format), ##__VA_ARGS__); \
+    if (Msg == "") \
+    { \
+        UE_LOG(SideScrollerLog, Log, TEXT("%s%s() : %s"), NETMODE_WORLD, FUNC_NAME, *GetNameSafe(this));\
+    } \
+    else \
+    { \
+        UE_LOG(SideScrollerLog, Log, TEXT("%s%s() : %s"), NETMODE_WORLD, FUNC_NAME, *Msg);\
+    } \
+    CLEAR_WARN_COLOR();\
+}
