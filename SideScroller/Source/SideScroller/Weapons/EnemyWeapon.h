080868cda5cd51b43ce88ad26f8c5d32acccceca
D00834\Daedalic <thorbenkohler@gmail.com>
2018-03-23 14:28:59 +0100

    removing ShotFired Implementable events due to GAS

--- SideScroller/Source/SideScroller/Weapons/EnemyWeapon.h
+++ SideScroller/Source/SideScroller/Weapons/EnemyWeapon.h
@@ -18,12 +18,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Can be used to assign visual effects and sounds
-	UFUNCTION(BlueprintImplementableEvent, Category = "PlayerWeapon")
-	void ShotFired();
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 };
