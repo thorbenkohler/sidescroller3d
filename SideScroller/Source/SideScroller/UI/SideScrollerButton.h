f5cafdf5b071ab42713eaf5f6011b0e6a25da801
D00834\Daedalic <thorbenkohler@gmail.com>
2018-03-26 10:51:35 +0200

    small clean up

--- SideScroller/Source/SideScroller/UI/SideScrollerButton.h
+++ SideScroller/Source/SideScroller/UI/SideScrollerButton.h
@@ -2,12 +2,9 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Components/Button.h"
 #include "SideScrollerButton.generated.h"
 
-class SButton;
-class USlateWidgetStyleAsset;
 
 UCLASS()
 class SIDESCROLLER_API USideScrollerButton : public UButton
