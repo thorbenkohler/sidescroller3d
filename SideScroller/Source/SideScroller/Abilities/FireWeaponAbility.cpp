0130ff8f964873158b6691f87f2140910aa612b9
D00834\Daedalic <thorbenkohler@gmail.com>
2018-05-04 15:49:14 +0200

    folder clean up. latest binaries.

--- SideScroller/Source/SideScroller/Abilities/FireWeaponAbility.cpp
+++ SideScroller/Source/SideScroller/Abilities/FireWeaponAbility.cpp
@@ -1,22 +1,4 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FireWeaponAbility.h"
-#include "Abilities/Utilities/AbilitySystemStatics.h"
-#include "Abilities/Utilities/AbilityDelegates.h"
-
-void UFireWeaponAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-                                         const FGameplayAbilityActorInfo* ActorInfo,
-                                         const FGameplayAbilityActivationInfo ActivationInfo,
-                                         const FGameplayEventData* TriggerEventData)
-{
-    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-
-    if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
-    {
-        EndAbility(Handle, ActorInfo, ActivationInfo, false, false);
-        return;
-    }
-
-    OnActivated();
-}
 
--- SideScroller/Source/SideScroller/Abilities/FireWeaponAbility.h
+++ SideScroller/Source/SideScroller/Abilities/FireWeaponAbility.h
@@ -9,14 +9,4 @@ UCLASS()
 class SIDESCROLLER_API UFireWeaponAbility : public USideScrollerGameplayAbility
 {
     GENERATED_BODY()
-
-public:
-    //~ Begin UGameplayAbility Interface
-    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
-                                 const FGameplayAbilityActivationInfo ActivationInfo,
-                                 const FGameplayEventData* TriggerEventData) override;
-    //~ End UGameplayAbility Interface
-
-    UFUNCTION(BlueprintImplementableEvent, Category = "FireWeaponAbility")
-    void OnActivated();
 };
